b975d6ce1db816636c47aca321f028cf693d94cb
Jairo Junior <jairo.ajunior@mercadolivre.com>
2022-04-11 16:57:51 -0300

    hr-worker finished

---
+++ hr-worker/src/main/kotlin/com/enterprise/domain/worker/entity/Worker.kt
@@ -0,0 +1,7 @@
+package com.enterprise.domain.worker.entity
+
+data class Worker (
+    val id: Long,
+    val name: String,
+    val dailyIncome: Double
+)
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into worker (name, daily_income) values ("Jairo", 150.00);
+insert into worker (name, daily_income) values ("Junior", 105.00);
+insert into worker (name, daily_income) values ("Pedro", 100.00);
